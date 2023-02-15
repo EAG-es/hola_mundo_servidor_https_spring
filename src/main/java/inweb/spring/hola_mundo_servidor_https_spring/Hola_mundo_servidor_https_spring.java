89a6c4982746a74c3c65dd63c61d4e21b4b118f7
emilio <emilio@aguilar.es>
2023-02-15 12:35:37 +0000

    20230215

--- src/main/java/inweb/spring/hola_mundo_servidor_https_spring/Hola_mundo_servidor_https_spring.java
+++ src/main/java/inweb/spring/hola_mundo_servidor_https_spring/Hola_mundo_servidor_https_spring.java
@@ -1,16 +1,14 @@
 package inweb.spring.hola_mundo_servidor_https_spring;
 
-import innui.modelos.configuraciones.ResourceBundles;
 import innui.modelos.errores.oks;
 import innui.modelos.modelos;
-import inweb.spring.hola_mundo_servidor_https_spring.controlador.pagina_inicio;
+import inweb.spring.hola_mundo_servidor_https_spring.controlador.Pagina_inicio;
 import inweb.spring.servidor_https_spring.Servidor_https_spring;
-import inweb.spring.servidor_https_spring.controlador.pagina_inicio_factoria;
+import inweb.spring.servidor_https_spring.controlador.Pagina_inicio_factoria;
 import static java.lang.System.exit;
-import java.util.ResourceBundle;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // Opcional
 public class Hola_mundo_servidor_https_spring extends Servidor_https_spring {
 
     public static void main(String[] args) {
@@ -18,7 +16,6 @@ public class Hola_mundo_servidor_https_spring extends Servidor_https_spring {
         try {
             Hola_mundo_servidor_https_spring servidor_https_spring 
                     = new Hola_mundo_servidor_https_spring();
-            // Object [] extra_array = { args };
             servidor_https_spring.run(ok, Hola_mundo_servidor_https_spring.class, args);
         } catch (Exception e) {
             ok.setTxt(e);
@@ -27,18 +24,18 @@ public class Hola_mundo_servidor_https_spring extends Servidor_https_spring {
             System.err.println(ok.txt);
             exit(1);
         }
-//		SpringApplication.run(Servidor_https_spring.class, args);
+        // Quitado:        
+        //   SpringApplication.run(Servidor_https_spring.class, args);
     }
 
     @Override
     public boolean run(oks ok, Object... extra_array) throws Exception {
         try {
             if (ok.es == false) { return ok.es; }
-            ResourceBundle in;
             iniciar(ok);
             if (ok.es) {
                 while (true) {
-                    pagina_inicio_factoria.setPagina_inicio(new pagina_inicio());
+                    Pagina_inicio_factoria.setPagina_inicio(new Pagina_inicio());
                     if (ok.es == false) { break; }
                     super.run(ok, extra_array);
                     break;
--- src/main/java/inweb/spring/hola_mundo_servidor_https_spring/controlador/Con_hola_mundo_servidor_https_spring.java
+++ src/main/java/inweb/spring/hola_mundo_servidor_https_spring/controlador/Con_hola_mundo_servidor_https_spring.java
@@ -1,40 +1,37 @@
 package inweb.spring.hola_mundo_servidor_https_spring.controlador;
 
 import innui.modelos.configuraciones.ResourceBundles;
-import innui.modelos.configuraciones.Resources;
 import innui.modelos.errores.oks;
 import innui.modelos.internacionalizacion.tr;
-import static inweb.spring.servidor_https_spring.controlador.pagina_inicio_factoria.k_ok_txt;
-import static inweb.spring.servidor_https_spring.controlador.pagina_inicio_factoria.k_titulo_txt;
-import java.io.File;
-import java.net.URL;
 import java.util.ResourceBundle;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import static inweb.spring.servidor_https_spring.controlador.Pagina_inicio_factoria.k_titulo_tex;
+import static inweb.spring.servidor_https_spring.controlador.Pagina_inicio_factoria.k_ok_tex;
 
 /**
  *
  * @author emilio
  */
 @Controller
-public class con_hola_mundo_servidor_https_spring {
+public class Con_hola_mundo_servidor_https_spring {
     public static String k_in_ruta = "in/inweb/spring/hola_mundo_servidor_https_spring/controlador/in";
-    public static String k_hola_mundo = "hola_mundo";
-    public static String k_index_html = k_hola_mundo + "/index.html";
+    public static final String k_hola_mundo = "hola_mundo"; // Ruta Web desde la raiz del servidor HTTPS
+    public static String k_index_html = k_hola_mundo + "/index.html"; // Ruta + Página Web de inicio
     
-    @GetMapping("hola_mundo")
+    @GetMapping(k_hola_mundo)
     public String hola_mundo(Model model) throws Exception {
         oks ok = new oks();
         ResourceBundle in;
         try {
             in = ResourceBundles.getBundle(k_in_ruta);
-            String texto = tr.in(in, "¡Hola Mundo! ");
-            model.addAttribute(k_titulo_txt, texto);
+            String texto = tr.in(in, "¡Hola Mundo! "); // Traducible a otros lenguajes
+            model.addAttribute(k_titulo_tex, texto); // Paso de datos (clave, valor)
         } catch (Exception e) {
             in = ResourceBundles.getBundle(k_in_ruta);
             ok.setTxt(tr.in(in, "Excepción inesperada "), e);
-            model.addAttribute(k_ok_txt, ok.getTxt());
+            model.addAttribute(k_ok_tex, ok.getTxt());
         }
         return k_index_html;
     }
