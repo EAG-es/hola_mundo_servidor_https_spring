7a708755bac12ac0303e80284ac7388b1832a6d2
emilio <emilio@aguilar.es>
2023-02-15 12:35:37 +0000

    20230215

--- src/main/java/inweb/spring/hola_mundo_servidor_https_spring/controlador/Pagina_inicio.java
+++ src/main/java/inweb/spring/hola_mundo_servidor_https_spring/controlador/Pagina_inicio.java
@@ -5,22 +5,19 @@
 package inweb.spring.hola_mundo_servidor_https_spring.controlador;
 
 import innui.modelos.errores.oks;
-import static inweb.spring.hola_mundo_servidor_https_spring.controlador.con_hola_mundo_servidor_https_spring.k_hola_mundo;
-import inweb.spring.servidor_https_spring.controlador.pagina_inicio_factoria;
-import java.util.ResourceBundle;
+import static inweb.spring.hola_mundo_servidor_https_spring.controlador.Con_hola_mundo_servidor_https_spring.k_hola_mundo;
+import inweb.spring.servidor_https_spring.controlador.Pagina_inicio_factoria;
 import org.springframework.ui.Model;
 
 /**
  *
  * @author emilio
  */
-public class pagina_inicio extends pagina_inicio_factoria {
-    public static String k_in_ruta = "in/inweb/spring/hola_mundo_servidor_https_spring/controlador/in";
+public class Pagina_inicio extends Pagina_inicio_factoria {
 
     @Override
     public String presentar_inicio(Model model, oks ok, Object... extra_array) throws Exception {
-        ResourceBundle in;
-        String text = "redirect:" + k_hola_mundo;
+        String text = "redirect:" + k_hola_mundo; // Orden de redirección a la página hola_mundo
         return text;
     }
 
